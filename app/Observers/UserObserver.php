ad2b1b4e052a29f27525dd94d4b030037381a69e
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2023-07-16 22:50:57 +0600

    Update UserObserver.php
    Fix Bug.
    previously role save when update the user every time.

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Enums\UserEnum;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -10,9 +11,13 @@ class UserObserver
 
     public function saving(User $user): void
     {
-        if (request()->has('password')){
-            $user->password = Hash::make(request()->password );
+        if (request()->has('password')) {
+            $user->password = Hash::make(request()->password);
         }
-        $user->assignRole(request()->roles ?? 'student');
+
+        if ($user->roles->isEmpty() || !request()->has('roles')) {
+            $user->assignRole(request()->roles ?? UserEnum::STUDENT->value);
+        }
     }
+
 }
