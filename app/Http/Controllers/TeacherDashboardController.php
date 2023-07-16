d6f23bd4b9296e6bbdee70c3df588b0400d5ae66
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2023-07-16 06:16:44 +0600

    Update TeacherDashboardController.php
    add Cache

--- app/Http/Controllers/TeacherDashboardController.php
+++ app/Http/Controllers/TeacherDashboardController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\UserEnum;
 use App\Http\Resources\Admin\UserResource;
-use App\Models\Result;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -19,20 +16,19 @@ class TeacherDashboardController extends Controller
     private function getData(): array
     {
         $cacheKey = 'user_dashboard:' . auth()->id();
-
-//        $data = Cache::get($cacheKey);
         $data = [];
-
         if (!$data) {
             $teacher_id = auth()->user()->id;
             $userQuery = User::query()
-                ->whereHas('teachers', function ($query) use ($teacher_id) {
+                ->whereHas('teachers', static function ($query) use ($teacher_id) {
                     $query->where('teacher_id', $teacher_id);
                 });
-            $examTakenCount = $userQuery->selectRaw('users.id, SUM((SELECT COUNT(*) FROM results WHERE results.user_id = users.id)) AS total_results')
-                ->groupBy('users.id')
-                ->pluck('total_results')
-                ->sum();
+            $examTakenCount = Cache::remember($cacheKey, now()->addMinute(5), function () use ($userQuery) {
+                return $userQuery->selectRaw('users.id, SUM((SELECT COUNT(*) FROM results WHERE results.user_id = users.id)) AS total_results')
+                    ->groupBy('users.id')
+                    ->pluck('total_results')
+                    ->sum();
+            });
             $students = UserResource::collection(
                 $userQuery->select('id', 'first_name', 'last_name', 'email', 'deleted_at')
                     ->filter(request()->only('search', 'trashed', 'column', 'direction'))
@@ -42,12 +38,8 @@ class TeacherDashboardController extends Controller
 
             $totalStudent = $students->total();
 
-            // Store the data in cache
             $data = compact('totalStudent', 'examTakenCount', 'students');
-
-            // Cache the data for a specific duration (e.g., 60 minutes)
             $data['loading'] = false;
-            Cache::put($cacheKey, $data, now()->addMinute(5));
         }
 
 
