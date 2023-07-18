3301f74a9c2e67fad6c8a449cd76a60d90be6fce
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2023-07-18 12:06:35 +0600

    Local seed only added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Result;
 use App\Models\School;
+use App\Models\Topic;
 use Illuminate\Database\Seeder;
 
 
@@ -22,11 +23,20 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             PermissionSeeder::class,
         ]);
+        if (app()->environment() === "local") {
+            /**
+             * Seed the categories.
+             */
+            Topic::create(['title' => 'Science']);
+            Topic::create(['title' => 'Mathematics']);
+            Topic::create(['title' => 'Physics']);
 
-        School::factory()->count(10)->create();
-        $this->call(UserTableSeeder::class);
-        $this->call(QuestionSeeder::class);
+            School::factory()->count(10)->create();
+            $this->call(UserTableSeeder::class);
+            $this->call(QuestionSeeder::class);
 
-        Result::factory()->count(600)->create();
+            Result::factory()->count(600)->create();
+
+        }
     }
 }
