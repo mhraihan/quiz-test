d43ea0ed7fa4c11496bd24d4ec97b4d9ff3fc405
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2023-07-31 11:14:48 +0600

    Faker chinese Language support

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -3,7 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Faker\Factory as Faker;
+use FakerChineseLorem\Provider\zh_CN\Lorem as ChineseLorem;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
  */
@@ -13,23 +14,26 @@ class QuestionFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
+     * @throws \Exception
      */
 
     public function definition()
     {
+        $faker = Faker::create();
+        $faker->addProvider(new ChineseLorem($faker));
         return [
             'title' => fake()->sentence(),
-            'title_two' => fake()->sentence(),
+            'title_two' => $faker->sentence(),
             'details' => fake()->paragraphs(random_int(1,6),true),
-            'details_two' => fake()->paragraphs(random_int(1,6),true),
+            'details_two' => $faker->paragraphs(random_int(1,6),true),
             'explain' => fake()->paragraphs(random_int(0,6),true),
-            'explain_two' => fake()->paragraphs(random_int(0,6),true),
+            'explain_two' => $faker->paragraphs(random_int(0,6),true),
             'correct_answer' =>  fake()->randomElement(['a','b','c','d']),
             'category_id' => random_int(1,4),
             'user_id' => random_int(1,1),
             'topic_id' => random_int(1,3),
             'options' => ["a" => fake()->word(), "b" => fake()->word(), "c" => fake()->word(),"d" => fake()->word()],
-            'options_two' => ["a" => fake()->word(), "b" => fake()->word(), "c" => fake()->word(),"d" => fake()->word()]
+            'options_two' => ["a" => $faker->word(), "b" => $faker->word(), "c" => $faker->word(),"d" => $faker->word()]
         ];
     }
 }
